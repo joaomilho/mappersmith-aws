2d6c306fe4ed6940350bb9abc37b5b0db7443105
Juan Lulkin <maiz@lulk.in>
2020-10-13 08:58:42 +0200

    Adds proper distribution

--- crypto.ts
+++ crypto.ts
@@ -2,6 +2,16 @@ import * as _sha256 from "crypto-js/sha256"
 import * as _hmac from "crypto-js/hmac-sha256"
 import * as CryptoJS from "crypto-js"
 
-export const sha256 = _sha256
-export const hmac = _hmac
 export type Hash = CryptoJS.lib.WordArray
+
+interface HasherHelper {
+  (message: Hash | string, cfg?: object): Hash
+}
+
+export function sha256(message: Hash | string): Hash {
+  return _sha256(message)
+}
+
+export function hmac(message: Hash | string, key: Hash | string): Hash {
+  return _hmac(message, key)
+}
